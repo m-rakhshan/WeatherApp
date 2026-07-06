cfb51190dbde0dffe66ee490559268fac5c500c3
Rakhshan M <113133160+m-rakhshan@users.noreply.github.com>
2026-07-06 21:35:19 +0500

    Update apis.js

--- src/constants/apis.js
+++ src/constants/apis.js
@@ -1,4 +1,4 @@
 const URLS = {
-  WEATHER_API: (latitude, longitude) => `http://api.weatherapi.com/v1/current.json?key=e23f9fb184bc4b1a8ff124910230808&q=${latitude},${longitude}`,
+  WEATHER_API: (latitude, longitude) => `http://api.weatherapi.com/v1/current.json?key={API_KEY}&q=${latitude},${longitude}`,
 };
 module.exports = { URLS };
